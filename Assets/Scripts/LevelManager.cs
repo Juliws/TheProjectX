e8d5077ba74481bc6f713606c073dc9f98ff9ba3
mpzrueda <69437182+mpzrueda@users.noreply.github.com>
2023-05-15 13:19:00 -0500

    Menu Update and Navigation
    Implement Menu buttons and BG with navigation panels

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    //Codigo de cambio de escena🔁
+    public void ChangeScence(string Scena)
+    {
+        SceneManager.LoadScene(Scena);
+    }
+    //Boton de pausa 🛑
+    public void Stop() 
+    {
+        Time.timeScale = 0;
+        Debug.Log("Pausado");
+    }
+    //Boton de continuar ⏩
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Continuado");
+    }
+    //Boton de Reiniciar 🔁
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Reiniciado");
+    }
+    //Boton de Cerrar ❌
+    public void Cerrar()
+    {
+        Application.Quit();
+        Debug.Log("Cerrado");
+    }
+}
