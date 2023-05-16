959cf4f6290ad04f082bd75ee3f81ecc4c1d9deb
Juliws <julianjerz@gmail.com>
2023-05-16 12:16:37 -0500

    Check ground and other logic minor corrections

--- Assets/Scripts/MoverPersonaje.cs
+++ Assets/Scripts/MoverPersonaje.cs
@@ -18,6 +18,7 @@ public class MoverPersonaje : MonoBehaviour
 
     [Header("Salto")]
     [SerializeField] private float jumpForce = 15.0f;
+    [SerializeField] private GameObject checkGround;
     [SerializeField] private bool enelSuelo;
 
     [Header("Agachado")]
@@ -50,7 +51,7 @@ public class MoverPersonaje : MonoBehaviour
     private void MoverPlayer() // Metodo para mover el personaje
     {
         x = Input.GetAxisRaw("Horizontal"); //Obtiene el input para moverse horizontalmente
-        //y = Input.GetAxis("Vertical");
+        //y = Input.GetAxisRaw("Vertical");
 
         transform.Translate(x * Time.deltaTime * velocidadMovimiento, 0, 0); //Recibe el input para mover el personaje de manera horizontal
 
@@ -108,11 +109,27 @@ public class MoverPersonaje : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision) // condicion para ver si hay contacto con el suelo (para la logica del salto)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Roof"))
+        {
+            enelSuelo = false;
+            estaAgachado = true;
+            anim.SetBool("Crouch", true);
+            colliderAgachado.enabled = true;
+            colliderDePie.enabled = false;
+            Debug.Log("Si estoy debajo");
+        }
+        else
+        {
+            estaAgachado = false;
+            anim.SetBool("Crouch", false);
+            colliderAgachado.enabled = false;
+            colliderDePie.enabled = true;
+        }
+        /*if (collision.gameObject.CompareTag("Ground"))
         {
             enelSuelo = true;
-        }
-        
+        }*/
+
         /*if (collision.gameObject.CompareTag("Ground"))
         {
             estaAgachado = true;
@@ -132,6 +149,11 @@ public class MoverPersonaje : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) // Metodo que determina despues de tomar el power up para cambiar el estado a verdadero
     {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            enelSuelo = true;
+        }
+
         if (other.gameObject.CompareTag("PwrupJump"))
         {
             powerUpJump = true;
@@ -151,21 +173,7 @@ public class MoverPersonaje : MonoBehaviour
             Destroy(other.gameObject);
         }
         
-        if (other.CompareTag("Wall"))
-        {
-            enelSuelo = false;
-            estaAgachado = true;
-            anim.SetBool("Crouch", true);
-            colliderAgachado.enabled = true;
-            colliderDePie.enabled = false;
-            Debug.Log("Si estoy debajo");
-        }else
-        {
-            estaAgachado = false;
-            anim.SetBool("Crouch", false);
-            colliderAgachado.enabled = false;
-            colliderDePie.enabled = true;
-        }
+        
     }
     public void Jump() // Metodo de salto
     {
@@ -175,11 +183,12 @@ public class MoverPersonaje : MonoBehaviour
             enelSuelo = false;
             anim.SetTrigger("Jump");
         }
-       
+        
     }
 
     public void Agachado() // Metodo para activar y desactivar el area de un collider
     {
+        
         if (Input.GetKey(KeyCode.DownArrow))//Input.GetKey(KeyCode.S))
         {
             agachado = true;
@@ -209,7 +218,7 @@ public class MoverPersonaje : MonoBehaviour
 
     public void Ataque() // Metodo para atacar
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKey(KeyCode.JoystickButton3))
         {
             anim.SetTrigger("Attack");
         }
