13de2029c52e0d81fe2f6830399bf843314b6939
Juliws <julianjerz@gmail.com>
2023-05-24 11:06:15 -0500

    Minor code Changes in Pruebas Scene

--- Assets/Scripts/MoverPersonaje.cs
+++ Assets/Scripts/MoverPersonaje.cs
@@ -45,17 +45,22 @@ public class MoverPersonaje : MonoBehaviour
     void Update()
     {
         //MoverPlayer();
-        ComprobarPowerUps(); //Comprueba si ha adquirido los power ups para activar la habilidad correspondiente
-        
+        //ComprobarPowerUps(); //Comprueba si ha adquirido los power ups para activar la habilidad correspondiente
+
         //Correr();
         //Jump();
         //Ataque();
+       
     }
 
     private void FixedUpdate()
     {
+        if (GameManager.Instance.gameStates == GameStates.GameOver)
+        {
+            return;
+        }
         MoverPlayer();
-        //ComprobarPowerUps();
+        ComprobarPowerUps();
     }
 
     private void MoverPlayer() // Metodo para mover el personaje
